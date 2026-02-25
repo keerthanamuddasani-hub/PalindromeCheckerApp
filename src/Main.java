a531c305af39bda77ded7b4c9da214b1f3f08cc8
keerthanamuddasani-hub <keerthanamuddasani@gmail.com>
2026-02-25 18:16:06 +0530

    UC5: queue

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,31 @@ import java.util.LinkedList;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) {
+
         String word = "level";
+
         Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
+
+        // Enqueue and Push characters
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            queue.add(ch);
-            stack.push(ch);
+            queue.add(ch);     // FIFO
+            stack.push(ch);    // LIFO
         }
+
         boolean isPalindrome = true;
+
+        // Compare Dequeue vs Pop
         while (!queue.isEmpty()) {
             if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
         }
+
         if (isPalindrome) {
             System.out.println(word + " is a palindrome.");
         } else {
