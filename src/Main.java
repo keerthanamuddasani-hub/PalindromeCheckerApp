810bc5835c1727e303dfa0c0cee8baf5fbdce016
keerthanamuddasani-hub <keerthanamuddasani@gmail.com>
2026-02-25 18:05:46 +0530

    UC5

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Original string
         String word = "madam";
 
-        // Create Stack to store characters
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
         for (int i = 0; i < word.length(); i++) {
             stack.push(word.charAt(i));
         }
 
-        // Pop characters and build reversed string
         String reversed = "";
 
         while (!stack.isEmpty()) {
             reversed = reversed + stack.pop();
         }
 
-        // Compare original and reversed
         if (word.equals(reversed)) {
             System.out.println(word + " is a palindrome.");
         } else {
