39b145fe3f0ddd8de52d1711c0ccd8a04f274a7c
keerthanamuddasani-hub <keerthanamuddasani@gmail.com>
2026-02-25 18:26:15 +0530

    UC7:linkedlist

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,60 @@
-import java.util.Deque;
-import java.util.LinkedList;
-public class Main {
-    public static void main(String[] args) {
-        String word = "radar";
-        Deque<Character> deque = new LinkedList<>();
-        for (int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
+public class Main{
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
         }
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
+    }
+    public static void main(String[] args) {
+        String word = "level";
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < word.length(); i++) {
+            Node newNode = new Node(word.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
-        if (isPalindrome) {
+        if (isPalindrome(head)) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
         }
+    }
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node prev = null;
+        Node current = slow;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        Node firstHalf = head;
+        Node secondHalf = prev;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
     }
 }
