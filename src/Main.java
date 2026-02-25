1ff5d274a8b2d69686031119c9c282de483929ed
keerthanamuddasani-hub <keerthanamuddasani@gmail.com>
2026-02-25 18:01:19 +0530

    UC5:stack based

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,32 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-                String word = "radar";
-                char[] characters = word.toCharArray();
-                int start = 0;
-                int end = characters.length - 1;
 
-                boolean isPalindrome = true;
-                while (start < end) {
-                    if (characters[start] != characters[end]) {
-                        isPalindrome = false;
-                        break;
-                    }
-                    start++;
-                    end--;
-                }
-                if (isPalindrome) {
-                    System.out.println(word + " is a palindrome.");
-                } else {
-                    System.out.println(word + " is not a palindrome.");
-                }
-            }
+        // Original string
+        String word = "madam";
+
+        // Create Stack to store characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
+
+        // Pop characters and build reversed string
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        // Compare original and reversed
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
